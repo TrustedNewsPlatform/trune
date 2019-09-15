96b64caf61fbc17d2faa693e186dd05a64aa5dd8
Piotr Adamczyk <xeno@xeno.yt>
2019-09-15 11:32:23 +0200

    pwa

---
+++ precache-manifest.210acffdbc89d1700025c9d70b91560b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60aaff7d5ba155dbaecf0b979ccd9a33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5055b5e357d046558962",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "23c25176237dd3baae36",
+    "url": "./static/css/main.9a31c126.chunk.css"
+  },
+  {
+    "revision": "5055b5e357d046558962",
+    "url": "./static/js/2.65c269c6.chunk.js"
+  },
+  {
+    "revision": "23c25176237dd3baae36",
+    "url": "./static/js/main.0a2cf13e.chunk.js"
+  },
+  {
+    "revision": "07108a10a4491e833b42",
+    "url": "./static/js/runtime~main.bae6cc7e.js"
+  }
+]);
